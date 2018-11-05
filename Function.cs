d0ed7027c27652b4361931245f4139826e78ae4f
kalash34 <bartoszpolaszek@gmail.com>
2018-11-05 16:14:45 +0100

    Create Function.cs

---
+++ Function.cs
@@ -0,0 +1,15 @@
+public class Funkcja{
+public Funkcja{
+   double addition(double a, double b){
+   a=Console.Convert.ToDouble(Console.ReadLine());
+   b=Console.Convert.ToDouble(Console.ReadLine());
+   return a+b;
+   
+   }
+
+
+
+
+}
+
+}
